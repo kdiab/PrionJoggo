c1edfcfcac981947c0b0bb5b897c91f723bdabc9
Diab <khalid@diab.cc>
2024-02-16 22:05:34 -0500

    a

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# Twitch authentication settings for pubsub
+# Twitch authentication settings for pubsub and chat
+# DO NOT SHARE WITH ANYONE
 APP_ID = "your_app_id" # YOUR APP_ID
 APP_SECRET = "your_app_secret" # YOUR APP_SECRET
 BOT_APP_ID = "bot_app_id" # BOT ACCOUNT'S APP_ID
