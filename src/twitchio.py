033b260611151e8fa6224cd51a2e3c03fb655d70
Diab <khalid@diab.cc>
2024-02-29 20:24:53 -0500

    twitchio

--- src/twitchio.py
+++ src/twitchio.py
@@ -1,44 +1,22 @@
 from twitchio.ext import commands
 
 class Bot(commands.Bot):
-
     def __init__(self):
-        super().__init__(
-            token='YOUR_OAUTH_TOKEN',
-            client_id='YOUR_CLIENT_ID',
-            nick='YOUR_BOT_NICKNAME',
-            prefix='!',
-            initial_channels=['#channel']
-        )
+        super().__init__(token='ACCESS_TOKEN', prefix='!', initial_channels=['djkumbot'])
 
     async def event_ready(self):
-        print(f'Ready | {self.nick}')
+        print(f'Logged in as | {self.nick}')
+        print(f'User id is | {self.user_id}')
 
     async def event_message(self, message):
         if message.echo:
             return
-
+        print(message.content)
         await self.handle_commands(message)
 
-async def kiss(cmd: ChatCommand):
-    default_names = ["Loose_Caboose", "xaddy_", "ubaru", "vori", "widejuicy", "djkumboi", "illicxt_bamb", "teaghandi"]
-    kissed_user = random.choice(default_names)
-    users = tracker.get_top_users()
-    msg = random.choice(settings.KISS_MESSAGES)
-    if (len(users) > len(default_names)):
-        kissed_user = random.choice(users)
-    msg = msg.replace('{x}', cmd.user.name).replace('{y}', kissed_user)
-    await cmd.send(msg)
+    @commands.command()
+    async def kiss(self, ctx: commands.Context):
+        await ctx.send(f'Hello {ctx.author.name}!')
 
-    @commands.command(name='timeout')
-    async def timeout_user(self, ctx):
-        if not ctx.author.is_mod:
-            return  # Only allow mods to use this command
-
-        target_user = ctx.message.content.split(' ')[1]  # Assumes the command is like "!timeout username"
-        await ctx.channel.timeout(user=target_user, duration=300)  # Timeout duration in seconds (300 seconds = 5 minutes)
-        await ctx.send(f'{target_user} has been timed out for 5 minutes.')
-
-if __name__ == '__main__':
-    bot = Bot()
-    bot.run()
+bot = Bot()
+bot.run()
